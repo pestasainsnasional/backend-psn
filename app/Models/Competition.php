578e3f3e02aec2979dc733043b846a894320244d
Faqih Firman Pratama <faqihfirmanpratama@MacBook-Pro-Faqih.local>
2025-06-23 21:44:19 +0700

    feat: create model Competition

---
+++ app/Models/Competition.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Competition extends Model
+{
+    use HasFactory, HasUlids;
+
+ 
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'competition_types_id',
+        'name',
+        'description',
+        'rules',
+        'major',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [ 'is_active' => 'boolean', ];
+    }
+
+    public function competitionType(): BelongsTo
+    {
+        return $this->belongsTo(CompetitionType::class, 'competition_types_id');
+    }
+
+    public function registrations(): HasMany
+    {
+        return $this->hasMany(Registration::class);
+    }
+}
