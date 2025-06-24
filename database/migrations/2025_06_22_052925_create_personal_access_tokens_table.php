8dccf018bb4da0ebf75eacea586b5e725b2a45bb
unknown <anargya115@gmail.com>
2025-06-24 16:13:03 +0700

    update: change migration configuration

--- database/migrations/2025_06_22_052925_create_personal_access_tokens_table.php
+++ database/migrations/2025_06_22_052925_create_personal_access_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->ulid('id')->primary();
-            $table->morphs('tokenable');
+            $table->ulidMorphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
