45d6cb4c76afff19a7e54f931bd7689a4d64f60d
unknown <anargya115@gmail.com>
2025-06-24 16:15:24 +0700

    feat: setup breeze

---
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class SocialLoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+
+            $user = User::where('email', $googleUser->email)->first();
+
+            if ($user) {
+                $token = $user->createToken('auth-token-google')->plainTextToken;
+                return redirect(env('FRONTEND_URL') . '/auth/callback?token=' . $token);
+            } else {
+                $user = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                    'password' => bcrypt(Str::random(16)),
+                    'email_verified_at' => now(),
+                    
+                ]);
+
+                $user->assignRole('user');
+            }
+
+            $token = $user->createToken('auth-token-google')->plainTextToken;
+            return redirect(env('FRONTEND_URL') . '/auth/callback?token=' . $token);
+        } catch (\Exception $e) {
+            dd($e);
+            return redirect(env('FRONTEND_URL') . '/login?error=google_auth_failed');
+        }
+    }
+}
