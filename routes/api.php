086cab10101bc378917d2372d50fbf5a29280c07
unknown <anargya115@gmail.com>
2025-06-25 13:47:08 +0700

    feat: make get competition route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialLoginController;
+use App\Http\Controllers\CompetitionController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::get('/auth/google/redirect', [SocialLoginController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [SocialLoginController::class, 'handleGoogleCallback']);
+
+
+Route::controller(CompetitionController::class)->group(function () {
+    Route::get('/competitions', 'index');
+});
