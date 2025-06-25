4c5a3cf564e8b2a80caa2a7942d88657727e5759
unknown <anargya115@gmail.com>
2025-06-25 13:46:00 +0700

    feat: make get competition controller

---
+++ app/Http/Controllers/CompetitionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\CompetitionType;
+use App\Models\Competition;
+
+
+class CompetitionController extends Controller
+{
+    public function index(): JsonResponse{
+        $competitions = Competition::with('competitionType')->latest()->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $competitions,
+        ]);
+    }
+}
