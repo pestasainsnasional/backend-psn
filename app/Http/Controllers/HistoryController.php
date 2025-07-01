ff6f44f4d17a07cbec45b2982dbddc73ac422b14
unknown <anargya115@gmail.com>
2025-07-01 23:04:14 +0800

    feat: modification testimonial controller and add get latest season function

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompetitionHistory;
 use App\Models\History;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -9,12 +10,33 @@ use Illuminate\Http\JsonResponse;
 
 class HistoryController extends Controller
 {
-    public function listSeasons(): JsonResponse{
+    public function listSeasons(): JsonResponse
+    {
         $seasons = History::orderBy('season_year', 'desc')->select('id', 'season_year')->get();
 
         return response()->json([
             'success' => true,
             'data' => $seasons,
+        ]);
+    }
+
+    public function latestSeason(): JsonResponse
+    {
+        $latestSeason = History::query()->select('id', 'theme', 'description', 'season_year', 'total_participants', 'total_competitions', "created_at", "updated_at")->with([
+            'media' => function ($query) {
+                $query->select('id', 'model_type', 'model_id', 'uuid', 'collection_name', 'name', 'file_name', 'disk');
+            },
+            'competitionHistory' => function ($query) {
+                $query->select('id', 'history_id', 'competition_name', 'competition_type', 'winner_name', "winner_school", "rank", "created_at", "updated_at");
+            },
+            'competitionHistory.media' => function ($query) {
+                $query->select('id', 'model_type', 'model_id', 'uuid', 'collection_name', 'name', 'file_name', 'disk');
+            }
+        ])->orderBy('season_year', 'desc')->first();
+
+        return response()->json([
+            'success' => true,
+            'data' => $latestSeason,
         ]);
     }
 }
--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -9,8 +9,14 @@ use App\Models\Testimonial;
 class TestimonialController extends Controller
 {
     public function index(): JsonResponse{
-        $testimonials = Testimonial::latest()->get();
-
+         $testimonials = Testimonial::query()
+            ->select('id', 'name', 'description', 'institution', 'created_at', 'updated_at')
+            ->with(['media' => function ($query) {
+                $query->select('id', 'model_type', 'model_id', 'uuid', 'collection_name', 'name', 'file_name', 'disk');
+            }])
+            ->latest()
+            ->get();
+            
         return response()->json([
             'success' => true,
             'data' => $testimonials,
