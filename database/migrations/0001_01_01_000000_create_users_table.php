27bc50f512beb091d8783f770db8d8571f0bf8c3
unknown <anargya115@gmail.com>
2025-07-02 16:03:50 +0800

    fix: deleting drafts table and change it to seassions

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -26,20 +26,19 @@ return new class extends Migration
         });
         
     
-        Schema::create('drafts', function (Blueprint $table) {
-            $table->ulid('id')->primary(); 
-            $table->foreignUlid('user_id')->constrained()->onDelete('cascade');
-            $table->foreignUlid('competition_id');
-            $table->json('data'); 
-            $table->timestamps();
-            
-            $table->unique(['user_id', 'competition_id']);
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignUlid('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('drafts');
+        Schema::dropIfExists('sessions');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('users');
     }
