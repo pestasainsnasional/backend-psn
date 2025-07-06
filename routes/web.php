636d8eb04a1b33461254a96a88c430719f6d0db4
unknown <anargya115@gmail.com>
2025-07-06 18:51:54 +0800

    feat: auto redirect from / to /admin/login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::redirect('/', '/admin/login');
 
 require __DIR__.'/auth.php';
